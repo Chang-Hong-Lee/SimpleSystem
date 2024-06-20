676d6d6fc6a80d4f604a93ffd1037a031e18c431
李常宏 <shawnlee@lingchain.com>
2024-06-20 13:16:33 +0800

    2-2:修正錯誤

--- SimpleSystem/Calculator.cs
+++ SimpleSystem/Calculator.cs
@@ -3,6 +3,12 @@
     public class Calculator
     {
         /// <summary> 基本加法 </summary>
-        public int Add(int a, int b) => a + b;
+        public int Add(int a, int b)
+        {
+            checked
+            {
+                return a + b;
+            }
+        }
     }
 }
